913ab7ad11d6c0e79e0f7b0a74c656536b3ad983
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-06-17 22:16:52 +0300

    Update snapshot test for OurDev

--- src/tests/OurDevs.test.js
+++ src/tests/OurDevs.test.js
@@ -1,10 +1,18 @@
-/* import { render } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 import OurDevs from '../components/OurDevs';
+import { apiSlice } from '../redux/apiSlice';
 
 describe('OurDevs tests', () => {
   test('Snapshot test. Should render OurDevs correctly', () => {
-    const tree = render(<OurDevs />);
+    const tree = render(
+      <ApiProvider api={apiSlice}>
+        <Router>
+          <OurDevs />
+        </Router>
+      </ApiProvider>
+    );
     expect(tree).toMatchSnapshot();
   });
 });
-*/
