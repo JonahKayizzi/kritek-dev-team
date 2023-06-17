5650d65afd0b48f26cac4aa2376576ec6a4ba033
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-06-17 18:22:54 +0300

    Destructure getDevs hook in OurDevs component

--- src/components/OurDevs.js
+++ src/components/OurDevs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useGetDevsQuery } from '../redux/apiSlice';
 import DevForm from './DevForm';
 
 const OurDevs = () => {
@@ -6,7 +7,22 @@ const OurDevs = () => {
     console.log('Add Dev Item');
   };
 
+  const {
+    data: devs,
+    error,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useGetDevsQuery();
+
   let content;
+  if (isLoading) {
+    content = <div>Loading...</div>;
+  } else if (isSuccess) {
+    console.log(devs);
+  } else if (isError) {
+    content = <div>{error}</div>;
+  }
 
   return (
     <main className="flex flex-col py-6 bg-gray-50 px-24 shadow-lg shadow-slate-300">
