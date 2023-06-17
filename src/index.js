204375711ddbe87c065f22d33b3348da86aa22e7
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-06-17 18:26:56 +0300

    Add provider to the index

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { apiSlice } from './redux/apiSlice';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ApiProvider api={apiSlice}>
+      <Router>
+        <App />
+      </Router>
+    </ApiProvider>
   </React.StrictMode>,
 );
 
