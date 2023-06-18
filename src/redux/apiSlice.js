f5596be94724c1d6826566d0e0cea6ad27aeeb1e
Mac <mac@Macs-MacBook-Pro-360.local>
2023-06-18 15:33:27 +0300

    Update json server url from local to render deployment

--- src/redux/apiSlice.js
+++ src/redux/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://kritek-dev-data.onrender.com' }),
   tagTypes: ['Developers'],
   endpoints: (builder) => ({
     getDevs: builder.query({
